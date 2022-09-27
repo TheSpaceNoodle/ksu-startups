ce4c998cba6e7ce9d88cbcdb7505c9ea49646c86
Volodymyr Shcherbyna <aknojlogiya@gmail.com>
2022-09-27 03:12:26 +0300

    build

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'ksu-startups',
+    appId: '1:54384494160:web:8875a9982fe0fa29dab355',
+    storageBucket: 'ksu-startups.appspot.com',
+    apiKey: 'AIzaSyCIxHY2xtGSlGzIBHEEyWXbyIpnA_gs7v8',
+    authDomain: 'ksu-startups.firebaseapp.com',
+    messagingSenderId: '54384494160',
+    measurementId: 'G-ZZTZDEL2PL',
+  },
   production: false
 };
 
