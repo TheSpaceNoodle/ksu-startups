8072726e87a5033cf67957934c908e8affeb31af
Volodymyr Shcherbyna <aknojlogiya@gmail.com>
2022-11-02 22:36:02 +0200

    Welcome page template done

--- src/app/pages/welcome-page/welcome-page.component.ts
+++ src/app/pages/welcome-page/welcome-page.component.ts
@@ -11,15 +11,15 @@ export class WelcomePageComponent implements OnInit {
     {
       name: 'Rebecca',
       position:
-        'Rebecca was a solo and a secondary character in Cyberpunk: Edgerunners.',
-      info: 'Rebecca is a very sharp-tongued person, has a tendency to extremes and is unpredictable. She is often shown going all out in fights and in no way shying away from using violence or even killing. Nevertheless, Rebecca is a very loyal person and does everything for the crew of Maine and supports David where she can.',
+        '<span class="highlight">Rebecca</span> was a solo and a secondary character in <span class="highlight">Cyberpunk: Edgerunners</span>.',
+      info: '<span class="highlight">Rebecca</span> is a very sharp-tongued person, has a tendency to extremes and is unpredictable. She is often shown going all out in fights and in no way shying away from using violence or even killing. Nevertheless, Rebecca is a very loyal person and does everything for the crew of Maine and supports David where she can.',
       imgUrl: './assets/partner-placeholder.png',
     },
     {
       name: 'Rebecca',
       position:
-        'Rebecca was a solo and a secondary character in Cyberpunk: Edgerunners.',
-      info: 'Rebecca is a very sharp-tongued person, has a tendency to extremes and is unpredictable. She is often shown going all out in fights and in no way shying away from using violence or even killing. Nevertheless, Rebecca is a very loyal person and does everything for the crew of Maine and supports David where she can.',
+        '<span class="highlight">Rebecca</span> was a solo and a secondary character in <span class="highlight">Cyberpunk: Edgerunners</span>.',
+      info: '<span class="highlight">Rebecca</span> is a very sharp-tongued person, has a tendency to extremes and is unpredictable. She is often shown going all out in fights and in no way shying away from using violence or even killing. Nevertheless, Rebecca is a very loyal person and does everything for the crew of Maine and supports David where she can.',
       imgUrl: './assets/partner-placeholder.png',
     },
   ];
