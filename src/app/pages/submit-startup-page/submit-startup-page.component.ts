3c90a146a82010711661badc092f237f83a5d6a0
Volodymyr Shcherbyna <aknojlogiya@gmail.com>
2023-01-09 10:57:29 +0200

    Submit form somewhat done

--- src/app/pages/submit-startup-page/submit-startup-page.component.ts
+++ src/app/pages/submit-startup-page/submit-startup-page.component.ts
@@ -9,6 +9,7 @@ import { StorageService } from 'src/app/services/storage.service';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SubmitStartupPageComponent implements OnInit {
+  file: File | null = null;
   startupForm = new FormGroup({
     startupName: new FormControl(''),
     startupFinances: new FormControl(''),
@@ -16,17 +17,23 @@ export class SubmitStartupPageComponent implements OnInit {
     startupDescription: new FormControl(''),
     startupHistory: new FormControl(''),
   });
-  formFilled = false;
+  formFilled = this.startupForm.valid;
 
   constructor(private storageService: StorageService) {}
 
   onFileUpload(e: any) {
-    this.storageService.upload(e.target.files[0]);
-    console.log(this.startupForm.value);
-    this.formFilled = true;
+    this.file = e.target.files[0];
+    if (
+      this.file &&
+      !(this.file.name.endsWith('.png') || this.file.name.endsWith('.jpg'))
+    ) {
+      this.file = null;
+    }
+    console.log(this.file);
   }
 
   onSubmit(e: any) {
+    this.storageService.upload(e.target.files[0]);
     console.log(this.startupForm.value);
   }
 
